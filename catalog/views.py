7efb5f6a317740a4c377a6d772e44153beff8784
damodara <jay108@mail.ru>
2025-12-17 13:58:57 +0300

    создали базовый шаблон и настроили первоначальное отображение продуктов

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "product_list.html", context)
 
 
 def contacts(request):
