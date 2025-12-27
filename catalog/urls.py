95a60501396a24071fd0102f193884bee14edac1
damodara <jay108@mail.ru>
2025-12-27 14:11:36 +0300

    upd

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, home
+from catalog.views import contacts, product_list, product_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", home, name="home"),
+    path("", product_list, name="product-list"),
+    path("product/<int:pk>/", product_detail, name="product-detail"),
     path("contacts/", contacts, name="contacts"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,20 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Product
 
 
 # Create your views here.
-def home(request):
+def product_list(request):
     products = Product.objects.all()
     context = {"products": products}
     return render(request, "product_list.html", context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, "product_detail.html", context)
 
 
 def contacts(request):
