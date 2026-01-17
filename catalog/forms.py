120505ddc8a55f4633496a490f74caad2fe821b3
damodara <jay108@mail.ru>
2026-01-17 22:40:34 +0300

    создали валидацию

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,4 +1,5 @@
-from django.forms import ModelForm
+from django import forms
+from django.core.exceptions import ValidationError
 
 from catalog.models import Product
 
@@ -10,7 +11,36 @@ class StyleFormMixin:
             field.widget.attrs["class"] = "form-control"
 
 
-class ProductForm(StyleFormMixin, ModelForm):
+class ProductForm(StyleFormMixin, forms.ModelForm):
+    FORBIDDEN_WORDS = [
+        "казино", "криптовалюта", "крипта", "биржа",
+        "дешево", "бесплатно", "обман", "полиция", "радар"
+    ]
+
     class Meta:
         model = Product
         fields = "__all__"
+
+    def clean_name(self):
+        name = self.cleaned_data.get("name")
+        if name:
+            name_lower = name.lower()
+            for word in self.FORBIDDEN_WORDS:
+                if word in name_lower:
+                    raise ValidationError(f"Название не может содержать слово '{word}'.")
+        return name
+
+    def clean_description(self):
+        description = self.cleaned_data.get("description")
+        if description:
+            description_lower = description.lower()
+            for word in self.FORBIDDEN_WORDS:
+                if word in description_lower:
+                    raise ValidationError(f"Описание не может содержать слово '{word}'.")
+        return description
+
+    def clean_price(self):
+        price = self.cleaned_data.get("price")
+        if price is not None and price < 0:
+            raise ValidationError("Цена не может быть отрицательной.")
+        return price
