455ed3773f8c9f5d9f11deb8ea4f98f15d34be6f
damodara <jay108@mail.ru>
2026-01-23 18:19:35 +0300

    создал модель User с миграцией

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "catalog",
     "blog.apps.BlogConfig",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,5 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Email')
+    phone = models.CharField(max_length=11, unique=True, verbose_name='Телефон', null=True, blank=True)
+    country = models.TextField(verbose_name="Страна", null=True, blank=True, max_length=50)
+    avatar =models.ImageField(upload_to='users/avatars', null=True, blank=True, verbose_name="Аватар")
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+
+    def __str__(self):
+        return self.email
+
