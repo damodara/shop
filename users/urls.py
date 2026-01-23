c730680684637eb7c3dff605bc13261f9df45800
damodara <jay108@mail.ru>
2026-01-23 19:38:27 +0300

    настроил регистрацию

--- users/urls.py
+++ users/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from django.views.generic import RedirectView
 
 from users.apps import UsersConfig
+from users.views import UserCreateView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path("login/", LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("regicter/", UserCreateView.as_view(), name="register"),
 ]
