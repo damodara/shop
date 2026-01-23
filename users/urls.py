1eea39c8051f79682152bf7336e9aced3a0579f2
damodara <jay108@mail.ru>
2026-01-23 19:31:00 +0300

    настроил вход\выход

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from django.views.generic import RedirectView
 
@@ -8,4 +8,5 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
